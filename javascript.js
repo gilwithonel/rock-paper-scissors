5dd4aea0d74f3300935173d90406ce933f8acecd
Isaiah Gilmer <ijgilmer106@gmail.com>
2022-03-12 16:35:32 -0500

    Created playRound function, used extensive if else if loop. Attempted to use ternary operator, but could not make it work.

--- javascript.js
+++ javascript.js
@@ -8,7 +8,27 @@ function computerPlay() {
         return "scissors"
     };
 }
+let choice = computerPlay();
 
 let input = prompt("Rock, paper, or scissors?")
 let playerSelection = input.toLowerCase();
-console.log(playerSelection);
+
+function playRound(computerPlay, playerSelection) {
+    if (choice == playerSelection) { 
+        return "Tie!"
+    } else if (choice == "rock" && playerSelection == "paper") {
+        return "You win! Paper beats rock"
+    } else if (choice == "paper" && playerSelection == "paper") {
+        return "You lose! Paper beats rock"
+    } else if (choice == "scissors" && playerSelection == "rock") {
+        return "You lose! Rock beats scissors"
+    } else if (choice == "rock" && playerSelection == "scissors") {
+        return "You win! Rock beats scissors"
+    } else if (choice == "paper" && playerSelection == "scissors") {
+        return "You win! Scissors beats paper"
+    } else if (choice == "scissors" && playerSelection == "paper") {
+        return "You lose! Scissors beats paper"
+    };
+};
+
+console.log(playRound(computerPlay, playerSelection));
