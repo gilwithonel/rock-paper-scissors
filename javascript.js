c5894e8d2fbcd98a37be13dda847ce9a4935d45c
Isaiah Gilmer <ijgilmer106@gmail.com>
2022-03-13 15:10:46 -0400

    Added game function, adjusted some variables to optomize performance.

--- javascript.js
+++ javascript.js
@@ -8,22 +8,25 @@ function computerPlay() {
         return "scissors"
     };
 }
-let choice = computerPlay();
+// let choice = computerPlay();
 
-let input = prompt()
-let playerSelection = input.toLowerCase();
+function playerChoice() {
+    let input = prompt()
+    let playerSelection = input.toLowerCase();
+    return playerSelection;
+};
 
-function playRound(computerPlay, playerSelection) {
+function playRound(choice, playerSelection) {
     if (choice == playerSelection) { 
         return "Tie!"
     } else if (choice == "rock" && playerSelection == "paper") {
         return "You win! Paper beats rock"
-    } else if (choice == "paper" && playerSelection == "paper") {
+    } else if (choice == "paper" && playerSelection == "rock") {
         return "You lose! Paper beats rock"
     } else if (choice == "scissors" && playerSelection == "rock") {
-        return "You lose! Rock beats scissors"
-    } else if (choice == "rock" && playerSelection == "scissors") {
         return "You win! Rock beats scissors"
+    } else if (choice == "rock" && playerSelection == "scissors") {
+        return "You lose! Rock beats scissors"
     } else if (choice == "paper" && playerSelection == "scissors") {
         return "You win! Scissors beats paper"
     } else if (choice == "scissors" && playerSelection == "paper") {
@@ -31,3 +34,12 @@ function playRound(computerPlay, playerSelection) {
     };
 };
 
+function game() {
+    for (let i = 0; i < 5; i++) {
+        let choice = computerPlay();
+        let playerSelection = playerChoice();
+        console.log(playRound(choice, playerSelection));
+    }
+}
+
+game();
